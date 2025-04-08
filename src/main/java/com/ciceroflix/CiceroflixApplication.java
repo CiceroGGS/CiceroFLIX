4d847b8989abc30f33caa0f3c638d5cf92471b24
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-08 09:09:26 -0300

    first commit

---
+++ src/main/java/com/ciceroflix/CiceroflixApplication.java
@@ -0,0 +1,13 @@
+package com.ciceroflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiceroflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiceroflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciceroflix/CiceroflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciceroflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiceroflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
