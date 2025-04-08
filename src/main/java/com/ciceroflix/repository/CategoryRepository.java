190d6ed505a000fdd7faa4c2a2f0dc518675e42c
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-08 09:14:55 -0300

    feat: Criada entidade Category e interface CategoryRepository

---
+++ src/main/java/com/ciceroflix/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ciceroflix.repository;
+
+import com.ciceroflix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
