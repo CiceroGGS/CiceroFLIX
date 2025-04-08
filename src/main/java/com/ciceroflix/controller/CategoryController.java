5ea6f3205016cb1b81746ebe2060fd686de3f028
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-08 09:17:55 -0300

    feat: Criados arquivoc CategoryController e CategoryService

---
+++ src/main/java/com/ciceroflix/controller/CategoryController.java
@@ -0,0 +1,7 @@
+package com.ciceroflix.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CategoryController {
+}
---
+++ src/main/java/com/ciceroflix/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.ciceroflix.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+}
