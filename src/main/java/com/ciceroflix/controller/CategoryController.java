7fdb357b9bc8674c2b72105601830ef3dd456f45
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-09 09:09:20 -0300

    feat: cCriados endpoints para CRUD de category

--- src/main/java/com/ciceroflix/controller/CategoryController.java
+++ src/main/java/com/ciceroflix/controller/CategoryController.java
@@ -1,10 +1,41 @@
 package com.ciceroflix.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ciceroflix.entity.Category;
+
+import com.ciceroflix.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ciceroflix/category")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    /************************************************************/
+    @GetMapping()
+    public List<Category> getAllCategories(){
+        return categoryService.findAll();
+    }
+    /************************************************************/
+    @PostMapping()
+    public Category saveCategory(@RequestBody Category category){
+        return categoryService.saveCategory(category);
+    }
+    /************************************************************/
+    @GetMapping("/{id}")
+    public Category getByCategoryId(@PathVariable Long id) {
+        return categoryService.findById(id);
+    }
+    /************************************************************/
+    @DeleteMapping("/{id}")
+    public void deleteCategoryById(@PathVariable Long id) {
+        categoryService.deleteById(id);
+    }
+    /************************************************************/
+
 }
--- src/main/java/com/ciceroflix/service/CategoryService.java
+++ src/main/java/com/ciceroflix/service/CategoryService.java
@@ -1,7 +1,52 @@
 package com.ciceroflix.service;
 
+import com.ciceroflix.entity.Category;
+import com.ciceroflix.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.repository= categoryRepository;
+    }
+
+    /************************************************************/
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    /************************************************************/
+
+    public Category saveCategory(Category category){
+        return repository.save(category);
+    }
+
+    /************************************************************/
+
+    public Category findById(Long id) {
+        Optional<Category> optCategory = repository.findById(id);
+        if(optCategory.isPresent()){
+            return optCategory.get();
+        } else {
+            return null;
+        }
+    }
+
+    /************************************************************/
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
 }
